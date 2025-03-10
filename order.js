5c034f6423991e9cb4644ce28e663107240a5bf0
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-03-10 16:11:21 -0500

    Update order.js

--- order.js
+++ order.js
@@ -22,27 +22,27 @@ const vars2 = ['strinput', 'fortinput', 'agilinput', 'intinput', 'willinput', 'c
 
 function change(p1, p2) {
     window[p1] = Math.max(0, Math.min(100, Math.floor(document.getElementById(p2).value)));
-    if ((330 - (str + fort + agil + int + will + char + hw + mw + lw + flm + ice + ltn + gale + shdw + bld + mtl)) >= 1) {
+    if (!(Number.isNaN(window[p1]))) {
+        if ((330 - (str + fort + agil + int + will + char + hw + mw + lw + flm + ice + ltn + gale + shdw + bld + mtl)) >= 1) {
     document.getElementById(p2).value = window[p1];
     } else {
         window[p1] = Math.min(100, (330 - (str + fort + agil + int + will + char + hw + mw + lw + flm + ice + ltn + gale + shdw + bld + mtl) + window [p1]));
+        document.getElementById(p2).value = window[p1];
     };
+    } else {
+        window[p1] = 0;
+        document.getElementById(p2).value = '';
+    };
+    if (window[p1] === 0) {
+        document.getElementById(p2).value = '';
+    }
     document.getElementById("invcount").innerHTML = 330 - (str + fort + agil + int + will + char + hw + mw + lw + flm + ice + ltn + gale + shdw + bld + mtl);
-    nullCheck(p1, p2);
 };
 
 function check(key) {
     if (key.code == "Enter") {
         num = vars2.indexOf(event.target.id);
         change(vars1[num], vars2[num]);
-    };
-};
-
-function nullCheck(x, y) {
-if (window[x] === 0 || Number.isNaN(window[x])) {
-    document.getElementById(y).value = '';
-} else {
-        document.getElementById(y).value = window[x];
     };
 };
 
