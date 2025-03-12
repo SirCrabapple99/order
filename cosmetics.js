439710277f4fdc3a982b8fa4cf54b0eb22bdfb76
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-03-12 13:40:23 -0500

    Update cosmetics.js

--- cosmetics.js
+++ cosmetics.js
@@ -7,3 +7,11 @@ function sendnotif(txt) {
 function hide() {
     document.getElementById("notif").style = "display: none;";
 }
+
+function show(tab) {
+    var x = document.getElementsByClassName("tab");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+    document.getElementById(tab).style.display = "block";
+};
